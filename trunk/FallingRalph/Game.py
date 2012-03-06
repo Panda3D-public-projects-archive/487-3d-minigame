3ef6841937ba7c64fe4de15e3a23dde665491953
delanepj@muohio.edu <delanepj@muohio.edu@ba00ffce-bf93-5a70-296b-c9b776c46451>
2012-03-06 22:57:07 +0000

    I added an avatar selection mode. It still needs text but it works overall. I will finish this later - Patrick

--- trunk/FallingRalph/Game.py
+++ trunk/FallingRalph/Game.py
@@ -26,6 +26,7 @@ from direct.task.Task import Task
 import math
 import copy
 from Player import *
+from Picker import Picker
 #import os
 #from Utilities import *
 
@@ -37,6 +38,10 @@ from Player import *
 
 
 #Constants
+#These are used in loading the avatars for the character selection screen.
+LIST_OF_AVATARS = [ "models/ralph", "models/sonic","models/tails","models/eve"]
+LIST_OF_SCALES = [ 1, .25,.25,1]
+LIST_OF_POSITIONS = [Vec3(-15,-5,0), Vec3(-7,-5,0), Vec3(7,-5,0), Vec3(15,-5,0)]
 
 
 class World(DirectObject):
@@ -62,10 +67,20 @@ class World(DirectObject):
 		base.camera.setPosHpr(Vec3(0,0,50), Vec3(0, -90, 0))
 		
 		
-		#Character Selection
-		self.avatarSelect()
-		base.camera.lookAt(self.player.avatar)
+		#Initialize Picker
+		self.picker = Picker(fromMask=BitMask32(0x01), collideWithGeom=True)
 		
+		#Character Selection
+		self.avatars = []
+		#Load the avatars on the screen for initial selection
+		for i in range(len(LIST_OF_AVATARS)):
+			self.avatars.append(self.loadAvatar(LIST_OF_AVATARS[i],LIST_OF_SCALES[i],LIST_OF_POSITIONS[i]))
+		#self.avatarSelect()
+		#base.camera.lookAt(self.player.avatar)
+		
+		#State Variables
+		self.notSelected = True
+		self.curAvatar = -1 #Used to determine which avatar to load
 		
 		'''
 		#A dictionary of what keys are currently being pressed
@@ -83,13 +98,16 @@ class World(DirectObject):
 		self.accept("h", self.displayHelp)
 		self.accept("space", self.keys.update, [{"levelStart":1}] )
 		'''
+		self.accept("mouse1", self.pick)
 		#Quit game
 		self.accept("escape", sys.exit)
 		
-		
+		self.avatarSelectTask = taskMgr.add(self.avatarSelect, "avatarSelect")
+		'''
 		#Register gameloop to the Task Manager
 		self.gameTask = taskMgr.add(self.gameLoop, "gameloop")
 		self.gameTask.last = 0
+		'''
 		
 		'''
 		#If we want music just comment out this part and put the file below
@@ -100,9 +118,42 @@ class World(DirectObject):
 		'''
 		
 		
-	def avatarSelect(self):
-		self.player = Player(SONIC)
-	
+	def avatarSelect(self,task):
+		if(self.curAvatar < 0):
+			return task.cont
+		else:
+			self.cleanUpAvatars()
+			self.player = Player(self.curAvatar)
+			self.ignore("mouse1") #We don't need picking anymore
+			#Now that the player has picked an avatar
+			#Prepare initial game state and add gameLoop to the task list.
+			self.gameTask = taskMgr.add(self.gameLoop, "gameloop")
+			self.gameTask.last = 0
+			
+			return task.done
+		#while(charNotSelected){
+			#self.player = Player(SONIC)
+		#}
+	def loadAvatar(self,path,scale,pos):
+		avatar = loader.loadModel(path)
+		avatar.setCollideMask(0x01)
+		avatar.reparentTo(render)
+		avatar.setScale(scale)
+		avatar.setPos(pos)
+		avatar.setP(-90)
+		return avatar
+	def cleanUpAvatars(self):
+		for i in range(len(LIST_OF_AVATARS)):
+			self.avatars[i].removeNode()
+	def pick(self):
+		cEntry = self.picker.pick()
+		if cEntry:
+			avatarGeomHit = cEntry.getIntoNodePath()
+			for i in range(len(self.avatars)):
+				p = self.avatars[i]
+				if p.isAncestorOf(avatarGeomHit):
+					self.curAvatar = i
+					return
 	def gameLoop(self, task):
 		#Getting the change in time since the last task.
 		dt = task.time - task.last
